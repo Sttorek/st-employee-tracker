d7e14bae33e770ae0731d558ed15d141d6f6aabe
Seth Torek <seth.torek@gmail.com>
2021-04-20 22:39:55 -0400

    added to readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # st-employee-tracker
+
+
+## Description
+This application will help a business keep track of their employees through a database. It is built with node js, Inquirer, and mysql.
+
+
+
+## Usage
+To use in terminal run npm i to install dependencies. Then run node index.js. Select what task you would like completed.
+
+
+## Link
+See the deployed site here
+[Heroku]("")
--- Schema.sql
+++ Schema.sql
@@ -4,27 +4,35 @@ CREATE DATABASE employee_tracker_db;
 
 USE employee_tracker_db;
 
-CREATE TABLE department (
-id INT,
-name VARCHAR(30),
-PRIMARY KEY (id)
-);
 
-CREATE TABLE role (
-id INT,
-title VARCHAR(30),
-salary DECIMAL,
-department_id int,
-PRIMARY KEY (id)
-);
+
+
 
 CREATE TABLE employee (
-id INT,
+id INT AUTO_INCREMENT NOT NULL,
 first_name VARCHAR(30),
 last_name VARCHAR(30),
 role_id INT,
 manager_id INT,
 PRIMARY KEY (id)
+);
+
+CREATE TABLE department (
+employee_id INT AUTO_INCREMENT NOT NULL,
+id INT,
+name VARCHAR(30),
+PRIMARY KEY (id),
+FOREIGN KEY (employee_id) REFERENCES employee(id)
+);
+
+CREATE TABLE role (
+employee_id INT AUTO_INCREMENT NOT NULL,
+id INT,
+title VARCHAR(30),
+salary DECIMAL,
+department_id int,
+PRIMARY KEY (id),
+FOREIGN KEY (employee_id) REFERENCES employee(id)
 );
 
 SELECT * FROM department;
